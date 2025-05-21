22e2824d8119dedd255d670a1f18cad8fc0068d3
Sirius <132120734+Sirius6907@users.noreply.github.com>
2025-05-21 16:00:32 +0530

    Delete frontend directory

--- frontend/src/store/useThemeStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("slivechat-theme") || "coffee",
-  setTheme: (theme) => {
-    localStorage.setItem("slivechat-theme", theme);
-    set({ theme });
-  },
-}));
